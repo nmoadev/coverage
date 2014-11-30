4873dece55fb994ce1d9daf60d7efea0660e6a51
nmoadev <alain.kuchta@gmail.com>
2014-11-30 02:11:56 -0500

    Fleshed out services more

--- public/js/services.js
+++ public/js/services.js
@@ -9,16 +9,30 @@ coverageServices.Cursor = function(){
         RIGHT: 'right',
         JUMP: 'jump'
     };
-    cursor.commandHistory = [];
-
+    cursor.commandList = [];
+    cursor.reset = function reset() {
+      cursor.commandList = [];
+    };
     angular.forEach(cursor.commandDictionary, function(value, key) {
         cursor[value] = function() {
-            cursor.commandHistory.push([key].concat(Array.prototype.slice.call(arguments)));
+            cursor.commandList.push([key].concat(Array.prototype.slice.call(arguments)));
             return cursor;
         }
     });
 
     return cursor;
+};
+
+coverageServices.Board = function() {
+  var board = {};
+  board.grid = [];
+  for (var i = 0; i < config.rows, i++) {
+      var row = [];
+      for (var j = 9; j < config.cols, i++) {
+          row.push(null);
+      }
+      board.grid.push(row);
+  };
 };
 
 coverageServices.factory('MatchSvc', ['$q', function($q){
@@ -31,11 +45,28 @@ coverageServices.factory('MatchSvc', ['$q', function($q){
    }
 }]);
 
-coverageServces.factory('BoardSvc', function(){
+coverageServces.factory('BoardSvc', ['SocketSvc', function(SocketSvc) {
+  var boards = {
+    test: {},
+    play: {}
+  };
 
+  // Register a listener for when the play board is updated
+  SocketSvc.on(SocketSvc.MessageTypes.Updates.PlayBoard, function(data) {
+    boards.play = data;
+  });
+
+  // Register a listener for when the test board is updated
+  SocketSvc.on(SocketSvc.MessageTypes.Updates.TestBoard, function(data) {
+    boards.test = data;
+  });
+  
+  return {
+    boards: boards
+  };
 });
 
-coverageServices.factory('SocketSvc', function() {
+coverageServices.factory('SocketSvc', ['$q', function($q) {
     var socket = io.connect();
     return {
         on: function (eventName, callback) {
@@ -57,9 +88,16 @@ coverageServices.factory('SocketSvc', function() {
             })
         }
     };
-});
+}]);
 
-coverageServices.factory('CommandSvc', [function() {
+/**
+ * Command Service
+ *
+ * Evals code using a Cursor object to create list of commands 
+ * on cursor.
+ *
+ */
+coverageServices.factory('CommandSvc', ['SocketSvc', function(SocketSvc) {
     var cursor = coverageServices.Cursor();
     return {
         submitCode: function(code) {
@@ -68,15 +106,25 @@ coverageServices.factory('CommandSvc', [function() {
               return err;
           }
 
-//          SocketSvc.send();
+          SocketSvc.emit(SocketSvc.MessageTypes.submitCode, {
+            actions : cursor.commandList
+          });
         },
         processCode: function(code) {
+            cursor.reset();
             try {
                 eval(code);
-                console.log(cursor.commandHistory)
             } catch (e) {
-
+              return e;    
             }
+        },
+        testCode: function(code) {
+          var err = this.processCode(code);
+          if (err) {
+              return err;
+          }
+
+          BoardSvc.applyCursor(cursor);
         }
     }
 }]);
